c3d69060b4e6aa4801c96a4c7603180f3f3c1891
Jake Chapman <quadanar@gmail.com>
2021-06-19 13:33:14 -0700

    asset additions from James.

---
+++ TileD/DinningRoomTileSet.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="DinningRoomTileSet" tilewidth="8" tileheight="8" tilecount="360" columns="20" backgroundcolor="#65ff00">
+ <grid orientation="orthogonal" width="16" height="8"/>
+ <image source="DinningRoomTileSet.png" width="160" height="144"/>
+</tileset>
