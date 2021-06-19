0dd7486b88676fb5a40d5d0d8cd716d2060d238c
Jake Chapman <quadanar@gmail.com>
2021-06-19 13:37:34 -0700

    add assets from Jake.

---
+++ TileD/TileSets/sample_town_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="sample_town" tilewidth="8" tileheight="8" tilecount="3136" columns="56">
+ <image source="../Sinking Port/assets/backgrounds/sample_town.png" width="448" height="448"/>
+</tileset>
---
+++ TileD/TileSets/stars.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="stars" tilewidth="8" tileheight="8" tilecount="1024" columns="32">
+ <image source="../Sinking Port/assets/backgrounds/stars.png" width="256" height="256"/>
+</tileset>
