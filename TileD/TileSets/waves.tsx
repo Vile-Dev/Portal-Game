5bb0b4b9a6b2a55eadea8508baba5ecaae035795
Jake Chapman <quadanar@gmail.com>
2021-06-20 00:47:54 -0700

    beginning of the last level

---
+++ TileD/TileSets/waves.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="waves" tilewidth="8" tileheight="8" tilecount="8" columns="8">
+ <image source="../waves.png" width="64" height="8"/>
+</tileset>
