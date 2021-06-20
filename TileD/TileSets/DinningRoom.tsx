6d753a61969b4417d0657cf8eb5e193c1fc319b0
Jake Chapman <quadanar@gmail.com>
2021-06-20 06:23:53 -0700

    basic game complete

---
+++ TileD/TileSets/DinningRoom.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="DinningRoom" tilewidth="8" tileheight="8" tilecount="360" columns="20">
+ <image source="../../assets/backgrounds/DinningRoom.png" width="160" height="144"/>
+</tileset>
--- TileD/TileSets/Free Furniture 2.tsx
+++ TileD/TileSets/Free Furniture 2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.0" name="Free Furniture 2" tilewidth="8" tileheight="8" tilecount="360" columns="20">
- <image source="../Free Furniture.png" trans="000000" width="160" height="144"/>
+<tileset version="1.5" tiledversion="1.7.0" name="Free Furniture Revert" tilewidth="8" tileheight="8" tilecount="360" columns="20">
+ <image source="../Free Furniture Revert.png" width="160" height="144"/>
 </tileset>
