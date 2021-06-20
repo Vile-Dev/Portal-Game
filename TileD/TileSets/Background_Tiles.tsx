8b1be21834b1cfb0f9c38cd6896ebe5adb5d28ba
Jake Chapman <quadanar@gmail.com>
2021-06-19 22:59:57 -0700

    add obstacles to beginning rooms and dialog.

--- TileD/TileSets/Background_Tiles.tsx
+++ TileD/TileSets/Background_Tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.0" name="Background_Tiles" tilewidth="8" tileheight="8" tilecount="196" columns="14">
- <image source="Background_Tiles.png.png" width="112" height="112"/>
+ <image source="../Background_Tiles.png.png" width="112" height="112"/>
 </tileset>
